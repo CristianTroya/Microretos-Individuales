9edb97eefa1c5c3ffa3777345c0376f19be2b702
Cristian Troya <ctromor410@g.educaand.es>
2026-03-05 09:20:22 +0100

    hover y center

---
+++ validar_2_2_1.js
@@ -0,0 +1,10 @@
+console.log("Terminal de [Tu Nombre] iniciada con éxito");
+console.warn("Acceso de desarrollador detectado");
+
+const boton = document.querySelector("#btn-modo");
+
+boton.addEventListener("click", function() {
+    // Alterna la clase 'noche' en todo el cuerpo de la web
+    document.body.classList.toggle("noche");
+    console.log('cualquier cosa')
+});
